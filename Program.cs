179f2f051a1902c00a08b99f1648675705b86d49
123Studios - Alex <93049425+123Studios@users.noreply.github.com>
2021-11-06 15:05:39 -0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace ObfuscatorAIO
         public static void Start()
         {
             Logo();
-            Console.WriteLine("\nC#, HTML, And JavaScript Obfuscation Is Coming Soon Aswell With Stronger Obfuscation Methods.\nI am not responsible for malicious use of this program.\n\n1.)PHP\n2.)Batch/MS-DOS Scripting Obfuscation\n3.)URL Obfuscation");
+            Console.WriteLine("\nHTML, And JavaScript Obfuscation Is Coming Soon Aswell With Stronger Obfuscation Methods.\nI am not responsible for malicious use of this program.\n\n1.)PHP\n2.)Batch/MS-DOS Scripting Obfuscation\n3.)URL Obfuscation\n4.)C-Sharp Obfuscation");
             Console.Write("\nPlease Select An Option =>");
             switch(Console.ReadLine())
             {
@@ -28,13 +28,11 @@ namespace ObfuscatorAIO
                 case "3":
                     URL.Start();
                 break;
+                case "4":
+                    CSharp.Start();
+                break;
             }
         }
         public static void Logo() { Console.ForegroundColor = ConsoleColor.Magenta; Console.WriteLine("    ____  __    ____                      __             ___    ________ \n" + @"   / __ \/ /_  / __/_  ________________ _/ /_____  _____/   |  /  _/ __ \  " + "\n " + @" / / / / __ \/ /_/ / / / ___/ ___/ __ `/ __/ __ \/ ___/ /| |  / // / / /" + "\n " + "/ /_/ / /_/ / __/ /_/ (__  ) /__/ /_/ / /_/ /_/ / /  / ___ |_/ // /_/ / " + "\n " + @"\____/_.___/_/  \__,_/____/\___/\__,_/\__/\____/_/  /_/  |_/___/\____/    Created By 123Studios" + "\n\n"); Console.ForegroundColor = ConsoleColor.White; }
-        /*/
-        /*/
-                                                                        
-
-
     }
 }
---
+++ Protection/CSharp.cs
@@ -0,0 +1,98 @@
+﻿using dnlib.DotNet;
+using dnlib.DotNet.Emit;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObfuscatorAIO
+{
+    class CSharp
+    {
+        private static Random random = new Random();
+        public static String JunkData(int length)
+        {
+            const string chars = "abcdefghijklmnopqrstuvwxyz";
+            return new string(Enumerable.Repeat(chars, length)
+            .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        public static void Start()
+        {
+            Console.Clear();
+            Program.Logo();
+            Console.Write("\n\nEnter Path To C# Executable=> ");
+            string path = Console.ReadLine();
+
+            byte[] array = File.ReadAllBytes(path);
+            AssemblyDef assemblyDef = AssemblyDef.Load(array);
+            ModuleDefMD moduleDefMD = ModuleDefMD.Load(array);
+
+            Console.WriteLine("[+] Read Executable Successfully");
+            obfuscate_strings(moduleDefMD);
+            MethodRename(moduleDefMD);
+            moduleDefMD.Write(assemblyDef.Name + "-obfuscated.exe");
+            Console.WriteLine("[+] Obfuscated Successfully!\nPress Any Key To Continue...");
+            Console.ReadKey();
+            Console.Clear();
+            Program.Start();
+
+        }
+        public static void MethodRename(ModuleDefMD moduleDef)
+        {
+            foreach (TypeDef typeDef in moduleDef.GetTypes())
+            {
+                foreach (MethodDef methodDef in from x in typeDef.Methods
+                                                where !x.IsConstructor && !x.IsVirtual
+                                                select x)
+                {
+                    bool flag = methodDef.Name == "1                             2                                       3                                4                                 5                            6                         7                                  8                                 9 ";
+                    if (flag)
+                    {
+                        return;
+                    }
+                    methodDef.Name = JunkData(300 + 20);
+                }
+            }
+        }
+        public static void obfuscate_strings(ModuleDef md) //Taken From Net-Obfuscate Created By BinaryScary
+        {
+            //foreach (var type in md.Types) // only gets parent(non-nested) classes
+
+            // types(namespace.class) in module
+            foreach (var type in md.GetTypes())
+            {
+                // methods in type
+                foreach (MethodDef method in type.Methods)
+                {
+                    // empty method check
+                    if (!method.HasBody) continue;
+                    // iterate over instructions of method
+                    for (int i = 0; i < method.Body.Instructions.Count(); i++)
+                    {
+                        // check for LoadString opcode
+                        // CIL is Stackbased (data is pushed on stack rather than register)
+                        // ref: https://en.wikipedia.org/wiki/Common_Intermediate_Language
+                        // ld = load (push onto stack), st = store (store into variable)
+                        if (method.Body.Instructions[i].OpCode == OpCodes.Ldstr)
+                        {
+                            // c# variable has for loop scope only
+                            String regString = method.Body.Instructions[i].Operand.ToString();
+                            String encString = Convert.ToBase64String(UTF8Encoding.UTF8.GetBytes(regString));
+                            Console.WriteLine($"{regString} -> {encString}");
+                            // methodology for adding code: write it in plain c#, compile, then view IL in dnspy
+                            method.Body.Instructions[i].OpCode = OpCodes.Nop; // errors occur if instruction not replaced with Nop
+                            method.Body.Instructions.Insert(i + 1, new Instruction(OpCodes.Call, md.Import(typeof(System.Text.Encoding).GetMethod("get_UTF8", new Type[] { })))); // Load string onto stack
+                            method.Body.Instructions.Insert(i + 2, new Instruction(OpCodes.Ldstr, encString)); // Load string onto stack
+                            method.Body.Instructions.Insert(i + 3, new Instruction(OpCodes.Call, md.Import(typeof(System.Convert).GetMethod("FromBase64String", new Type[] { typeof(string) })))); // call method FromBase64String with string parameter loaded from stack, returned value will be loaded onto stack
+                            method.Body.Instructions.Insert(i + 4, new Instruction(OpCodes.Callvirt, md.Import(typeof(System.Text.Encoding).GetMethod("GetString", new Type[] { typeof(byte[]) })))); // call method GetString with bytes parameter loaded from stack 
+                            i += 4; //skip the Instructions as to not recurse on them
+                        }
+                    }
+                    //method.Body.KeepOldMaxStack = true;
+                }
+            }
+        }
+    }
+}
