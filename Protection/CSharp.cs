7e7aa08d4b470ee3ed0c1734386807b9a9abc8e9
123Studios - Alex <93049425+123Studios@users.noreply.github.com>
2021-11-07 19:42:37 -0800

    Add files via upload

--- Protection/CSharp.cs
+++ Protection/CSharp.cs
@@ -4,8 +4,10 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Media;
+using OpCodes = dnlib.DotNet.Emit.OpCodes;
 
 namespace ObfuscatorAIO
 {
@@ -18,18 +20,22 @@ namespace ObfuscatorAIO
             return new string(Enumerable.Repeat(chars, length)
             .Select(s => s[random.Next(s.Length)]).ToArray());
         }
+
+
         public static void Start()
         {
             Console.Clear();
             Program.Logo();
             Console.Write("\n\nEnter Path To C# Executable=> ");
             string path = Console.ReadLine();
+            if (path.Contains("\"")) { path = path.Replace("\"", ""); }
 
             byte[] array = File.ReadAllBytes(path);
             AssemblyDef assemblyDef = AssemblyDef.Load(array);
             ModuleDefMD moduleDefMD = ModuleDefMD.Load(array);
 
             Console.WriteLine("[+] Read Executable Successfully");
+            AddStringMethod(moduleDefMD);
             obfuscate_strings(moduleDefMD);
             MethodRename(moduleDefMD);
             moduleDefMD.Write(assemblyDef.Name + "-obfuscated.exe");
@@ -38,6 +44,15 @@ namespace ObfuscatorAIO
             Console.Clear();
             Program.Start();
 
+        }
+        public static MethodDef Clonesignature(MethodDef from, MethodDef to)
+        {
+            to.Attributes = from.Attributes;
+
+            if (from.IsHideBySig)
+                to.IsHideBySig = true;
+
+            return to;
         }
         public static void MethodRename(ModuleDefMD moduleDef)
         {
@@ -52,44 +67,59 @@ namespace ObfuscatorAIO
                     {
                         return;
                     }
-                    methodDef.Name = JunkData(300 + 20);
+                    methodDef.Name = JunkData(300);
                 }
             }
         }
         public static void obfuscate_strings(ModuleDef md) //Taken From Net-Obfuscate Created By BinaryScary
         {
-            //foreach (var type in md.Types) // only gets parent(non-nested) classes
 
-            // types(namespace.class) in module
             foreach (var type in md.GetTypes())
             {
                 // methods in type
                 foreach (MethodDef method in type.Methods)
                 {
-                    // empty method check
                     if (!method.HasBody) continue;
-                    // iterate over instructions of method
                     for (int i = 0; i < method.Body.Instructions.Count(); i++)
                     {
-                        // check for LoadString opcode
-                        // CIL is Stackbased (data is pushed on stack rather than register)
-                        // ref: https://en.wikipedia.org/wiki/Common_Intermediate_Language
-                        // ld = load (push onto stack), st = store (store into variable)
+
                         if (method.Body.Instructions[i].OpCode == OpCodes.Ldstr)
                         {
-                            // c# variable has for loop scope only
                             String regString = method.Body.Instructions[i].Operand.ToString();
                             String encString = Convert.ToBase64String(UTF8Encoding.UTF8.GetBytes(regString));
-                            // methodology for adding code: write it in plain c#, compile, then view IL in dnspy
-                            method.Body.Instructions[i].OpCode = OpCodes.Nop; // errors occur if instruction not replaced with Nop
+                            method.Body.Instructions[i].OpCode = OpCodes.Nop; 
                             method.Body.Instructions.Insert(i + 1, new Instruction(OpCodes.Call, md.Import(typeof(System.Text.Encoding).GetMethod("get_UTF8", new Type[] { })))); // Load string onto stack
-                            method.Body.Instructions.Insert(i + 2, new Instruction(OpCodes.Ldstr, encString)); // Load string onto stack
+                            method.Body.Instructions.Insert(i + 2, new Instruction(OpCodes.Ldstr, encString)); 
                             method.Body.Instructions.Insert(i + 3, new Instruction(OpCodes.Call, md.Import(typeof(System.Convert).GetMethod("FromBase64String", new Type[] { typeof(string) })))); // call method FromBase64String with string parameter loaded from stack, returned value will be loaded onto stack
                             method.Body.Instructions.Insert(i + 4, new Instruction(OpCodes.Callvirt, md.Import(typeof(System.Text.Encoding).GetMethod("GetString", new Type[] { typeof(byte[]) })))); // call method GetString with bytes parameter loaded from stack 
-                            i += 4; //skip the Instructions as to not recurse on them
+                            i += 4; 
                         }
                     }
-                    //method.Body.KeepOldMaxStack = true;
+                    
+                }
+            }
+        }
+        public static void AddStringMethod(ModuleDefMD module)
+        {
+            foreach (TypeDef typeDef in module.Types)
+            {
+                bool isGlobalModuleType = typeDef.IsGlobalModuleType;
+                if (!isGlobalModuleType)
+                {
+                    for (int i = 0; i < random.Next(30, 60); i++)
+                    {
+                        CorLibTypeSig @string = typeDef.Module.CorLibTypes.String;
+                        MethodDef methodDef = new MethodDefUser(JunkData(2000), MethodSig.CreateStatic(@string), MethodImplAttributes.IL, MethodAttributes.FamANDAssem | MethodAttributes.Family | MethodAttributes.Static | MethodAttributes.HideBySig)
+                        {
+                            Body = new CilBody()
+                        };
+                        typeDef.Methods.Add(methodDef);
+                        for (int j = 0; j < random.Next(10, 33); j++)
+                        {
+                            methodDef.Body.Instructions.Add(new Instruction(OpCodes.Ldc_I4, random.Next(99999999, 999999999)));
+                        }
+                        methodDef.Body.Instructions.Add(new Instruction(OpCodes.Ret));
+                    }
                 }
             }
         }
